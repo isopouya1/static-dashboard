dee9edfdb9d30fa4c88b5b987c3e402574d8b598
isopouya <pouya2402@gmail.com>
2026-02-21 19:36:41 +0330

    add chart.js on project

--- public/app.js
+++ public/app.js
@@ -1,18 +1,18 @@
-import Chart from 'chart.js'
-
-const ctx = document.getElementById('myChart');
+ const ctx = document.getElementById('myChart');
 
   new Chart(ctx, {
     type: 'bar',
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
+        backgroundColor: '#E3F5FF',
         label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        data: [12, 29, 3, 5, 2, 3],
         borderWidth: 1
       }]
     },
     options: {
+      
       scales: {
         y: {
           beginAtZero: true
